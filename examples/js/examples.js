21eea0a5140c6029ad0c7264a28e5191e5408d8c
Alberto <a.delafuente@bi4group.com>
2014-09-28 09:08:35 +0200

    Created global.json with the styles variables, import change in index.html, and small change in examples.js

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -10,11 +10,5 @@ basicButtonObj.buttonSize = "big";
 basicButtonObj.buttonId = "basicButton-1";
 basicButtonObj.buttonClass = "basicButton";
 basicButtonObj.buttonText = "Basic";
-basicButtonObj.backgroundColor = "rgb(255,255,255)";
-basicButtonObj.borderColor = "rgb(239,148,19)";
-basicButtonObj.borderSize = "1px";
-basicButtonObj.borderType = "solid";
-basicButtonObj.textColor = "rgb(239,148,19)";
-basicButtonObj.textSize = "12px";
 
 var basicButton = addButton(basicButtonObj);
