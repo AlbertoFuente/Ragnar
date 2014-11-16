bbf1f5381da92b7d111481bd59d7f67a0b366644
Alberto <a.delafuente@bi4group.com>
2014-11-16 12:45:25 +0100

    Changes in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt){
 
     grunt.initConfig({
 
+        uglify: {
+            my_target: {
+                files: {
+                    '_/js/scripts.min.js': ['/vendor/*.js','/components/*.js','examples/js/*.js']
+                }
+            }
+        }
     });
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 };
