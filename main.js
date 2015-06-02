0d7494fe51974007de47a18d39a45c286536a53b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-02 21:44:11 +0200

    main.js with require modules

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-import buttons from "./components/buttons";
-import services from "./components/services";
+requirejs.config({
+    app_name: 'lab',
+    paths: {
+        // vendor
+        'jquery': '../node_modules/jquery/dist/jquery.min',
+        // scripts
+        'services': 'components/services',
+        'buttons': 'components/buttons',
+        'samples': 'examples/script/examples'
+  },
+  shim: {
+        'services': {
+            exports: 'services',
+        },
+        'buttons': {
+            exports: 'buttons'
+        },
+        'jquery': {
+            exports: 'jquery'
+        },
+        'samples': {
+            exports: 'samples'
+        }
+    }
+});
 
-
-export {
-    buttons,
-    services
-};
+require(['jquery', 'services', 'buttons', 'samples'], function($, services, buttons, samples){
+    'use strict';
+	samples.init();
+});
