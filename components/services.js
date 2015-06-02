7337ada007e446ec55b538b0068146ce0bc853de
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-02 21:44:51 +0200

    services.js changes for requirejs

--- components/services.js
+++ components/services.js
@@ -1,23 +1,24 @@
-(function() {
+define(function () {
     'use strict';
 
-    const getJsonData =  (url) => {
-		return new Promise((resolve, reject) => {
-			let req = new XMLHttpRequest();
-			req.open('GET', url);
+    return {
+        getJsonData: function(url) {
+            return new Promise(function(resolve, reject) {
+                var req = new XMLHttpRequest();
+                req.open('GET', url);
 
-			req.onload = () => {
-				if (req.status === 200) {
-					resolve(req.response);
-				} else {
-					reject(Error(req.statusText));
-				}
-			};
-			req.onerror = () => {
-				reject(Error(req.statusText));
-			};
-			req.send();
-		});
-	};
-    export getJsonData;
+                req.onload = function() {
+                    if (req.status === 200) {
+                        resolve(req.response);
+                    } else {
+                        reject(Error(req.statusText));
+                    }
+                };
+                req.onerror = function() {
+                    reject(Error(req.statusText));
+                };
+                req.send();
+            });
+        }
+    };
 }());
