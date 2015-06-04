c92621dd41a3bd4010cfe5d7c5c25f304340cfee
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-04 13:08:10 +0200

    main.js changes in dependencies

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 requirejs.config({
     app_name: 'lab',
     paths: {
+        // vendor
+        'jquery': 'node_modules/jquery/dist/jquery.min',
         // scripts
         'services': 'js/services',
         'buttons': 'js/buttons',
-        'components': 'js/components'
+        'components': 'js/components',
+        'samples': 'examples/script/examples.js'
     },
     shim: {
+        'jquery': {
+            exports: 'jquery'
+        },
         'services': {
             exports: 'services'
         },
@@ -15,11 +21,14 @@ requirejs.config({
         },
         'components': {
             exports: 'components'
+        },
+        'samples': {
+            exports: 'samples'
         }
     }
 });
 
-require(['services', 'buttons', 'components'], function(services, buttons, components) {
+require(['jquery', 'services', 'buttons', 'components', 'samples'], function($, services, buttons, components, samples) {
     'use strict';
     components.init();
 });
