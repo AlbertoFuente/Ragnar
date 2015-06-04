f6ec98d0ecf4843c251db8f17aab7a7e621a5653
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-04 10:40:04 +0200

    ES6 Promise

--- components/services.js
+++ components/services.js
@@ -1,24 +1,24 @@
-define('services', function () {
+define('services', function() {
     'use strict';
 
     return {
         getJsonData: function(url) {
-            return new Promise(function(resolve, reject) {
-                var req = new XMLHttpRequest();
+            return new Promise((resolve, reject) => {
+                let req = new XMLHttpRequest();
                 req.open('GET', url);
 
-                req.onload = function() {
+                req.onload = () => {
                     if (req.status === 200) {
                         resolve(req.response);
                     } else {
                         reject(Error(req.statusText));
                     }
                 };
-                req.onerror = function() {
+                req.onerror = () => {
                     reject(Error(req.statusText));
                 };
                 req.send();
             });
         }
     };
-}());
+});
